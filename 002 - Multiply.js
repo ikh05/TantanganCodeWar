d38e19e85dd4fa79c2b35cafa6749e70c9030a80
ikh05 <mi3061923002@gmail.com>
2024-01-30 12:55:31 +0800

    002/366 Multiply

---
+++ 002 - Multiply.js
@@ -0,0 +1,12 @@
+// 002 - Multiply
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// function multiply(a, b){
+//   a * b
+// }
+
+// function multiply(a, b){
+// 	return a * b;
+// }
+
+const multiply = (a, b) => a * b;
