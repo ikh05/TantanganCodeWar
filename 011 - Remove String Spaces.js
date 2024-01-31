0ef1735fb54a9b6509e82b64ee136be0094f84a6
ikh05 <mi3061923002@gmail.com>
2024-01-31 21:36:23 +0800

    011/366 - Remove String Spaces

---
+++ 011 - Remove String Spaces.js
@@ -0,0 +1,10 @@
+// 011 - Remove String Spaces
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// function noSpace(x){
+// 	return x.replaceAll(' ','');
+// }
+
+const noSpace = x => x.replaceAll(' ','');
+
+console.log(noSpace('8aaaaa dddd r     '))
