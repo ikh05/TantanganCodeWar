bacbb5fe4da108471ea9b16be8fd8fa62f8f5c16
ikh05 <mi3061923002@gmail.com>
2024-01-31 16:03:49 +0800

    008/366 - Opposite number

---
+++ 008 - Opposite number.js
@@ -0,0 +1,11 @@
+// 008 - Opposite number
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// function opposite(number) {
+//   return -number;
+// }
+
+
+const opposite = number => -number;
+
+console.log(opposite(5.4444))
