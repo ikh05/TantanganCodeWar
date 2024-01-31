a66ef24da123fbfaf383f4ba8a8fb2d2f2b202eb
ikh05 <mi3061923002@gmail.com>
2024-01-31 16:03:36 +0800

    007/366 - Convert a Number to a String!

---
+++ 007 - Convert a Number to a String!.js
@@ -0,0 +1,15 @@
+// 007 - Convert a Number to a String!
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// function numberToString(num) {
+//   return `${num}`;
+// }
+
+numberToString = String
+
+// const numberToString = num => String(num);
+// const numberToString = num => num.toString();
+// const numberToString = num => ''+num;
+// const numberToString = num => `${num}`;
+
+console.log(numberToString(-4000))
